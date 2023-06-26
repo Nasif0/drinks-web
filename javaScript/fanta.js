2dc404320ff5945508dc4f82feca12d404920e7d
Nasif.0 <princenasif.00@gmail.com>
2023-06-26 19:32:22 +0600

    commited

---
+++ javaScript/fanta.js
@@ -0,0 +1,15 @@
+// document.getElementById('change-fanta').addEventListener('click',function(){
+//      var fantaImage = document.querySelector('.fanta-img');
+//      fantaImage.src = 'Image/fanta.png';
+//      fantaImage.style.width = '360px'
+    
+
+//      var orangeImage = document.querySelector('.orange-img');
+//      orangeImage.src = 'Image/pngegg (1).png';
+//     document.getElementById('change-colour').style.backgroundColor = 'rgb(247,231,135)';
+//     document.getElementById('lipton-text').innerText ='S P R I T E'
+  
+    
+     
+           
+     
