5e651f25ec2e6c4b79c434a0e9d8388cc75a7b88
Nasif.0 <princenasif.00@gmail.com>
2023-06-26 18:09:12 +0600

    sprite change commit

---
+++ javaScript/sprite.js
@@ -0,0 +1,14 @@
+document.getElementById('change-sprite').addEventListener('click',function(){
+     var spriteImage = document.querySelector('.sprite-img');
+     spriteImage.src = 'Image/sprite.png';
+     spriteImage.style.width = '192px'
+     var orangeImage = document.querySelector('.orange-img');
+     orangeImage.src = 'Image/pngegg (1).png';
+    document.getElementById('change-colour').style.backgroundColor = 'rgb(247,231,135)';
+    document.getElementById('lipton-text').innerText ='S P R I T E'
+  
+    
+     
+           
+     
+});
