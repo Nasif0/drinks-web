2690de5c29517aa786360d9073488d57e628c5a0
Nasif.0 <princenasif.00@gmail.com>
2023-06-26 16:33:18 +0600

    commit

--- javaScript/Apple.js
+++ javaScript/Apple.js
@@ -1,3 +1,19 @@
-window.addEventListener('scroll', function(){
-     document.getElementById('lipton').
-})
+ document.getElementById('change-lipton').addEventListener('click',function(){
+var image = document.querySelector('#lipton');
+image.src = 'Image/lipton.png';
+var appleImage = document.querySelector('#apple');
+appleImage.src = 'Image/apple.png';
+appleImage.style.width = '550px'
+appleImage.style.marginTop = '-110px'
+appleImage.style.marginLeft = '-50px'
+
+document.getElementById('change-colour').style.backgroundColor ='rgb(234,115,115)';
+document.getElementById('lipton-text').innerText = 'L I P T O N';
+      
+
+ });
+
+// document.getElementById('change-lipton').addEventListener('click',function(){
+//       var appleImage = document.querySelector('#apple');
+//       appleImage.src = 'Image/pngegg (2)';
+// })
